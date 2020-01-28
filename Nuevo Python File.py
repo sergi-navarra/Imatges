74c1106c3b6165805621f4fd562be6aa4ed7f82c
sergi-navarra <60349735+sergi-navarra@users.noreply.github.com>
2020-01-28 10:59:53 +0100

    Update Nuevo Python File.py

--- Nuevo Python File.py
+++ Nuevo Python File.py
@@ -1,1 +1,7 @@
-print("Hola")
+def escriu_capçalera(nom_fitxer):
+    with open(nom_fitxer, mode="w") as imatge:
+        imatge.write('P1\n')
+        imatge.write('3 3\n')
+
+if __name__ == '__main__':
+    escriu_capçalera('imatge.pbm')
